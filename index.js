0a96f72069c5f9140b196311c75c1f099b836b10
dev-suryajith <suryajithss08@gmail.com>
2025-10-20 23:14:45 +0530

    Data

--- index.js
+++ index.js
@@ -1,22 +1,25 @@
-//1. import json server
-const JSONServer = require(`json-server`)
+// import the json server
+const JSONServer = require('json-server');
 
-// 2. creating server for running json
-const rBuilder = JSONServer.create()
+// create server for running json file
+const rBuilder = JSONServer.create();
 
-// 3. creating middleware
-const middleware = JSONServer.defaults()
+// create middleware
+const middleware = JSONServer.defaults();
 
-// 4. inport db.json
-const router = JSONServer.router("db.json")
+// import db.json file
+const router = JSONServer.router('db.json');
 
-// 5. Setting port to run the server
-const PORT = 3000 || process.env.PORT
+// define port to run the server
+const PORT = process.env.PORT || 3002;
 
-// 6. User middleware, server and listen for data from frontend
+// use middleware
+rBuilder.use(middleware);
 
-rBuilder.use(middleware)
-rBuilder.use(router)
-rBuilder.listen(PORT,()=>{
-    console.log("Resume Builder started running at port 3000");
-})
+// use router
+rBuilder.use(router);
+
+// start the server
+rBuilder.listen(PORT, () => {
+  console.log(`JSON Server running at PORT number ${PORT}`);
+});
